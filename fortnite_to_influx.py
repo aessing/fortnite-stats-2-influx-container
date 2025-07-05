25de508463edf4b1b6c96b69b2ef26ba4772f152
Andre Essing <21235652+aessing@users.noreply.github.com>
2025-07-05 22:35:36 +0200

    -

--- fortnite_to_influx.py
+++ fortnite_to_influx.py
@@ -69,6 +69,7 @@ import requests
 from influxdb_client import InfluxDBClient, Point, WritePrecision
 from influxdb_client.client.write_api import SYNCHRONOUS
 
+
 # =============================================================================
 # CONFIGURATION
 # =============================================================================
